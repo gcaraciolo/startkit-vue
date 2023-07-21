b86df09a472ce5e1c34b16a0a7389c1bf2d442a1
Guilherme Caraciolo <gcaraciolo@gmail.com>
2023-07-21 06:08:10 -0300

    vue startkit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <header>
+    <h1>Startkit Vue</h1>
+  </header>
+
+  <main class="bg-green-800 p-8">
+    <p>Go and make domething awesome!</p>
+  </main>
+</template>
+
+<style scoped lang="scss">
+header {
+  background-color: rgb(var(--primary-color));
+  color: #fff;
+  padding: 20px;
+  text-align: center;
+}
+</style>
